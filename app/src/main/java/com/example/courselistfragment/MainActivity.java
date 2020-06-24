69b268f5e2bfed513b894db45042ab1d8a229dbc
edwin001-tech <edwinshitakwa8@gmail.com>
2020-06-24 12:22:41 +0300

    created list fragment

---
+++ app/src/main/java/com/example/courselistfragment/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.courselistfragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.myContainer);
+
+        if (fragment == null) {
+            fragment = new myFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.myContainer, fragment)
+                    .commit();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/courselistfragment/data/Course.java
@@ -0,0 +1,34 @@
+package com.example.courselistfragment.data;
+
+import android.content.Context;
+
+public class Course {
+    private String courseName;
+    private String courseImage;
+
+    public Course(String courseName, String courseImage) {
+        this.courseName = courseName;
+        this.courseImage = courseImage;
+    }
+
+    public int getImageResourceId(Context context){
+        return context.getResources().getIdentifier(this.courseName, "drawable", context.getPackageName());
+
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseImage() {
+        return courseImage;
+    }
+
+    public void setCourseImage(String courseImage) {
+        this.courseImage = courseImage;
+    }
+}
